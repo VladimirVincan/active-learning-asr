0823b511571c03accd83569e5b49cfba22ce1076
Vladimir Vincan <vladimirvincan@gmail.com>
2024-03-13 16:33:35 +0100

    init Dataset Analyzer. WIP

---
+++ data/DatasetAnalyzer.py
@@ -0,0 +1,102 @@
+import os
+import shutil
+import sys
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import datasets
+import pandas as pd
+import soundfile
+from datasets import Audio, load_dataset
+from pydub import AudioSegment
+from transformers import HfArgumentParser
+
+from DatasetDownloader import DataArguments, DatasetDownloader
+
+
+class DatasetAnalyzer(DatasetDownloader):
+    def __init__(self, data_args):
+        super().__init__(data_args)
+        self._df = pd.read_csv(os.path.join(self._folder, self._csv))
+
+    def file_number(self):
+        files = os.listdir(os.path.join(self._folder, 'clips'))
+        num_files = len(files)
+        return num_files
+
+    def duration(self):
+        """
+        Returns the total duration of the audio folder in seconds.
+        """
+        total_duration_seconds = 0
+
+        for filename in os.listdir(os.path.join(self._folder, 'clips')):
+            if filename.endswith(self._extension):
+                audio = AudioSegment.from_file(os.path.join(self._folder, 'clips', filename))
+                total_duration_seconds += audio.duration_seconds
+
+        return total_duration_seconds
+
+    def speaker_counts(self, speaker_num=-1):
+        """
+        Prints the speaker id and number of audio files that the speaker appeared in.
+        """
+        speaker_counts = self._df[self._speaker_column].value_counts()
+        # if speaker_num >= 0:
+        #     # create a shorter dict with the top speaker_num speakers
+        #     speaker_counts = dict(list(speaker_counts.items())[:speaker_num])
+        import pprint
+        pprint.pprint(speaker_counts)
+        return speaker_counts
+
+    def duration_per_speaker(self, speaker_num=-1):
+        """
+        Returns a dictionary where key is speaker id and value is total duration in seconds.
+        """
+        duration_dict = {}
+        i = 0
+        for index, row in self._df.iterrows():
+            audio = AudioSegment.from_file(os.path.join(self._folder, 'clips', row[self._path_column]))
+            duration = audio.duration_seconds
+
+            if duration_dict.get(row[self._speaker_column]) is not None:
+                duration_dict[row[self._speaker_column]] += duration
+            else:
+                duration_dict[row[self._speaker_column]] = duration
+
+        # sort the dict
+        duration_dict = (sorted(duration_dict.items(), key=lambda item: item[1], reverse=True))
+        import pprint
+        pprint.pprint(duration_dict)
+
+        if speaker_num >= 0:
+            # create a shorter dict with the top speaker_num speakers
+            duration_dict = dict(list(duration_dict.items())[:speaker_num])
+
+        return duration_dict
+
+
+
+
+def main():
+    parser = HfArgumentParser((DataArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        data_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        data_args = parser.parse_args_into_dataclasses()
+    data_args = data_args[0]
+
+    dataset_analyzer = DatasetAnalyzer(data_args)
+
+    speaker_counts = dataset_analyzer.speaker_counts(20)
+    # print(speaker_counts)
+    # dataset_analyzer.duration_per_speaker(20)
+    # total_duration_seconds = dataset_analyzer.duration()
+    # print(f"Total duration of audio files in the folder: {total_duration_seconds:.2f} seconds")
+
+    # num_files = dataset_analyzer.file_number()
+    # print(f"Total number of audio files in the folder: {num_files}")
+
+
+if __name__ == '__main__':
+    main()
--- data/DatasetDownloader.py
+++ data/DatasetDownloader.py
@@ -57,6 +57,10 @@ class DataArguments:
         default='path',
         metadata={'help': 'Name of column name that has text labels of corresponding audio files.'}
     )
+    speaker_column: str = field(
+        default='speaker_id',
+        metadata={'help': 'Name of column name that has names/ids of speakers.'}
+    )
     csv: str = field(
         default='metadata.csv',
         metadata={'help': 'Name of csv file.'}
@@ -69,88 +73,89 @@ class DataArguments:
 
 class DatasetDownloader:
     def __init__(self, data_args):
-        self.__use_auth_token = data_args.use_auth_token
-        self.__dataset = data_args.dataset
-        self.__lang = data_args.lang
-        self.__split = data_args.split
-        self.__folder = os.path.join(os.getcwd(), data_args.folder)
-        self.__cache = os.path.join(os.getcwd(), data_args.folder + '_cache')
-        self.__sampling_rate = data_args.sampling_rate
-        self.__mono = data_args.mono
-        self.__extension = data_args.extension
-        self.__label_column = data_args.label_column
-        self.__path_column = data_args.path_column
-        self.__csv = os.path.join(self.__cache, data_args.csv)
-        self.__num_samples = data_args.num_samples
+        self._use_auth_token = data_args.use_auth_token
+        self._dataset = data_args.dataset
+        self._lang = data_args.lang
+        self._split = data_args.split
+        self._folder = os.path.join(os.getcwd(), data_args.folder)
+        self._cache = os.path.join(os.getcwd(), data_args.folder + '_cache')
+        self._sampling_rate = data_args.sampling_rate
+        self._mono = data_args.mono
+        self._extension = data_args.extension
+        self._label_column = data_args.label_column
+        self._path_column = data_args.path_column
+        self._speaker_column = data_args.speaker_column
+        self._csv = data_args.csv
+        self._num_samples = data_args.num_samples
 
-        datasets.config.DOWNLOADED_DATASETS_PATH = Path(str(self.__cache))
-        datasets.config.HF_DATASETS_CACHE = Path(str(self.__cache))
+        datasets.config.DOWNLOADED_DATASETS_PATH = Path(str(self._cache))
+        datasets.config.HF_DATASETS_CACHE = Path(str(self._cache))
 
-    def __create_folders(self, folder_path):
+    def _create_folders(self, folder_path):
         os.makedirs(folder_path)
 
-    def __remove_folders(self, folder_path):
+    def _remove_folders(self, folder_path):
         if os.path.exists(folder_path):
             shutil.rmtree(folder_path)
 
-    def __move_file(self, src_path, dest_path):
+    def _move_file(self, src_path, dest_path):
         shutil.move(src_path, dest_path)
 
-    def __move_folder(self, src_path, dest_path):
+    def _move_folder(self, src_path, dest_path):
         file_names = os.listdir(src_path)
         for file_name in file_names:
-            self.__move_file(os.path.join(src_path, file_name), dest_path)
+            self._move_file(os.path.join(src_path, file_name), dest_path)
 
-    def __process_librispeech(self, df):
-        df = df.rename(columns={"text": self.__label_column, "file": self.__path_column})
-        df[self.__path_column] = df[self.__path_column].apply(lambda x: x.split('.')[0] + self.__extension)
+    def _process_librispeech(self, df):
+        df = df.rename(columns={"text": self._label_column, "file": self._path_column, "speaker_id": self._speaker_column})
+        df[self._path_column] = df[self._path_column].apply(lambda x: x.split('.')[0] + self._extension)
         return df
 
-    def __preprocess_common_voice(self, row):
+    def _preprocess_common_voice(self, row):
         row['audio']['path'] = os.path.basename(row['audio']['path'])
         row['path'] = os.path.basename(row['audio']['path'])
         return row
 
-    def __process_common_voice(self, df):
-        df = df.rename(columns={"sentence": self.__label_column, "path": self.__path_column})
-        df[self.__path_column] = df[self.__path_column].apply(lambda x: x.split('.')[0] + self.__extension)
+    def _process_common_voice(self, df):
+        df = df.rename(columns={"sentence": self._label_column, "path": self._path_column, "client_id": self._speaker_column})
+        df[self._path_column] = df[self._path_column].apply(lambda x: x.split('.')[0] + self._extension)
         return df
 
     def download(self):
-        self.__remove_folders(self.__folder)
-        self.__remove_folders(self.__cache)
-        self.__create_folders(os.path.join(self.__folder, 'clips'))
-        self.__create_folders(os.path.join(self.__cache, 'clips'))
+        self._remove_folders(self._folder)
+        self._remove_folders(self._cache)
+        self._create_folders(os.path.join(self._folder, 'clips'))
+        self._create_folders(os.path.join(self._cache, 'clips'))
 
-        dataset = load_dataset(self.__dataset, self.__lang, split=self.__split, streaming=True, use_auth_token=self.__use_auth_token, cache_dir=self.__cache)
-        dataset = dataset.cast_column('audio', Audio(sampling_rate=self.__sampling_rate, mono=self.__mono))  # change sampling rate
+        dataset = load_dataset(self._dataset, self._lang, split=self._split, streaming=True, use_auth_token=self._use_auth_token, cache_dir=self._cache)
+        dataset = dataset.cast_column('audio', Audio(sampling_rate=self._sampling_rate, mono=self._mono))  # change sampling rate
         dataset = iter(dataset)
 
         rows = []
         for i, row in enumerate(dataset):
             print(i)
-            if self.__dataset.find('common_voice') >= 0:
-                row = self.__preprocess_common_voice(row)
-            path = os.path.join(self.__cache, 'clips', row['audio']['path'])
+            if self._dataset.find('common_voice') >= 0:
+                row = self._preprocess_common_voice(row)
+            path = os.path.join(self._cache, 'clips', row['audio']['path'])
             path = path.split('.')[0] + '.wav'
             soundfile.write(path, row['audio']['array'], row['audio']['sampling_rate'])
 
             del row['audio']
             rows.append(row)
 
-            if i == self.__num_samples:
+            if i == self._num_samples:
                 break
 
         df = pd.DataFrame(rows)
-        if self.__dataset.find('librispeech') >= 0:
-            df = self.__process_librispeech(df)
-        elif self.__dataset.find('common_voice') >= 0:
-            df = self.__process_common_voice(df)
-        df.to_csv(self.__csv, index=False, header=True)
+        if self._dataset.find('librispeech') >= 0:
+            df = self._process_librispeech(df)
+        elif self._dataset.find('common_voice') >= 0:
+            df = self._process_common_voice(df)
+        df.to_csv(os.path.join(self._cache, self._csv), index=False, header=True)
 
-        self.__move_file(self.__csv, self.__folder)
-        self.__move_folder(os.path.join(self.__cache, 'clips'), os.path.join(self.__folder, 'clips'))
-        self.__remove_folders(self.__cache)
+        self._move_file(os.path.join(self._cache, self._csv), self._folder)
+        self._move_folder(os.path.join(self._cache, 'clips'), os.path.join(self._folder, 'clips'))
+        self._remove_folders(self._cache)
 
 
 def main():
