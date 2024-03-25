23b10c0968c4bbba89cee11b04a154a47c90dbbb
Vladimir Vincan <vladimirvincan@gmail.com>
2024-03-25 11:17:40 +0100

    added 'split' functionality to TrainingCreator.py. added create_training_dataset.sh

--- data/TrainingCreator.py
+++ data/TrainingCreator.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import datasets
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from transformers import HfArgumentParser
 
 
@@ -21,11 +22,22 @@ class DataArguments:
     )
     split1: str = field(
         default='train',
-        metadata={'help': 'Options: train/dev/test/same. Same means keep the same distribution if the data is already split.'}
+        metadata={'help': 'Options: train/dev/test/same/split. Same means keep the same distribution if the data is already split.'}
     )
     split2: str = field(
         default='train',
-        metadata={'help': 'Options: train/dev/test/same. Same means keep the same distribution if the data is already split.'}
+        metadata={'help':
+                  'Options: train/dev/test/same/none. \
+                  Same means keep the same distribution if the data is already split. \
+                  None means use only csv1 to create audio subfolder.'}
+    )
+    split_size: float = field(
+        default= 0.3,
+        metadata={'help': 'Percentage that goes into test.'}
+    )
+    speaker_id_1: str = field(
+        default='',
+        metadata={'help': 'Single speaker id to incude from csv1.'}
     )
     folder: str = field(
         default='merge',
@@ -51,16 +63,23 @@ class TrainingCreator:
         self._csv2 = data_args.csv2
         self._split1 = data_args.split1
         self._split2 = data_args.split2
+        self._split_size = data_args.split_size
+        self._speaker_id_1 = data_args.speaker_id_1
         self._folder = data_args.folder
         self._label_column = data_args.label_column
         self._path_column = data_args.path_column
         self._speaker_column = data_args.speaker_column
 
         self._df1 = pd.read_csv(self._csv1)
-        self._df2 = pd.read_csv(self._csv2)
+        if self._speaker_id_1 != '':
+            self._df1[self._speaker_column] = self._df1[self._speaker_column].astype(str)
+            self._df1 = self._df1[self._df1[self._speaker_column] == self._speaker_id_1]
+        if self._split2 != 'none':
+            self._df2 = pd.read_csv(self._csv2)
 
     def _create_relative_symlink(self, src, dst):
         dir = os.path.dirname(dst)
+        src = os.path.realpath(src)
         src = os.path.relpath(src, dir)
         return os.symlink(src, dst)
 
@@ -68,6 +87,7 @@ class TrainingCreator:
         if os.path.exists(self._folder):
             shutil.rmtree(self._folder)
         os.makedirs(os.path.join(self._folder, 'clips'))
+
         if self._split1 == 'train' or self._split2 == 'train':
             os.mkdir(os.path.join(self._folder, 'clips', 'train'))
         if self._split1 == 'dev' or self._split2 == 'dev':
@@ -79,15 +99,21 @@ class TrainingCreator:
             dirname = os.path.dirname(self._csv1)
             splits = os.listdir(os.path.join(dirname, 'clips'))
             for split in splits:
-                if not os.path.exists(os.path.join(self._folder, 'clips', split)):
-                    os.mkdir(os.path.join(self._folder, 'clips', split))
+                dir = os.path.join(self._folder, 'clips', split)
+                if not os.path.exists(dir) and os.path.isdir(dir):
+                    os.mkdir(dir)
+
+        elif self._split1 == 'split':
+            os.makedirs(os.path.join(self._folder + '_split_1', 'clips'))
+            os.makedirs(os.path.join(self._folder + '_split_2', 'clips'))
 
         if self._split2 == 'same':
             dirname = os.path.dirname(self._csv2)
             splits = os.listdir(os.path.join(dirname, 'clips'))
             for split in splits:
-                if not os.path.exists(os.path.join(self._folder, 'clips', split)):
-                    os.mkdir(os.path.join(self._folder, 'clips', split))
+                dir = os.path.join(self._folder, 'clips', split)
+                if not os.path.exists(dir) and os.path.isdir(dir):
+                    os.mkdir(dir)
 
     def _symlink_csv(self, csv_dir, df, split):
         """
@@ -99,6 +125,18 @@ class TrainingCreator:
                 dst = os.path.join(self._folder, 'clips', split, row[self._path_column])
             elif split == 'same':
                 dst = os.path.join(self._folder, row[self._path_column])
+                """
+                TODO: remove after DatasetDownloader changes file names to relative file paths + file names
+                """
+                if row[self._path_column] == os.path.basename(row[self._path_column]):
+                    dst = os.path.join(self._folder, 'clips', row[self._path_column])
+            elif split.find('split') >= 0:
+                dst = os.path.join(self._folder + split, row[self._path_column])
+                """
+                TODO: remove after DatasetDownloader changes file names to relative file paths + file names
+                """
+                if row[self._path_column] == os.path.basename(row[self._path_column]):
+                    dst = os.path.join(self._folder + split, 'clips', row[self._path_column])
             else:
                 raise ValueError('Split name incorrect!')
 
@@ -110,13 +148,26 @@ class TrainingCreator:
         return df
 
     def _create_metadata(self):
-        concat_df = pd.concat([self._df1, self._df2], ignore_index=True)
+        if self._split1 == 'split':
+            self._df1.to_csv(os.path.join(self._folder + '_split_1', 'metadata.csv'), index=False)
+            self._df2.to_csv(os.path.join(self._folder + '_split_2', 'metadata.csv'), index=False)
+            return
+        if self._split2 != 'none':
+            concat_df = pd.concat([self._df1, self._df2], ignore_index=True)
+        else:
+            concat_df = self._df1
         concat_df.to_csv(os.path.join(self._folder, 'metadata.csv'), index=False)
 
     def create_training_data(self):
         self._create_filepath()
-        self._df1 = self._symlink_csv(os.path.dirname(self._csv1), self._df1, self._split1)
-        self._df2 = self._symlink_csv(os.path.dirname(self._csv2), self._df2, self._split2)
+        if self._split1 != 'split':
+            self._df1 = self._symlink_csv(os.path.dirname(self._csv1), self._df1, self._split1)
+        else:
+            train, test = train_test_split(self._df1, test_size=self._split_size)
+            self._df1 = self._symlink_csv(os.path.dirname(self._csv1), train, '_split_1')
+            self._df2 = self._symlink_csv(os.path.dirname(self._csv1), test, '_split_2')
+        if self._split2 != 'none':
+            self._df2 = self._symlink_csv(os.path.dirname(self._csv2), self._df2, self._split2)
         self._create_metadata()
 
 
---
+++ data/create_training_dataset.sh
@@ -0,0 +1,29 @@
+rm -rf ls_dump_split_1
+rm -rf ls_dump_split_2
+rm -rf ls_cv_train_dump
+rm -rf final
+
+python3 TrainingCreator.py \
+        --csv1="librispeech/metadata.csv" \
+        --split1="split" \
+        --split2="none" \
+        --split_size="0.3" \
+        --folder="ls_dump"
+
+python3 TrainingCreator.py \
+        --csv1="ls_dump_split_1/metadata.csv" \
+        --csv2="common_voice/metadata.csv" \
+        --split1="same" \
+        --split2="same" \
+        --folder="ls_cv_train_dump"
+
+python3 TrainingCreator.py \
+        --csv1="ls_cv_train_dump/metadata.csv" \
+        --csv2="ls_dump_split_2/metadata.csv" \
+        --split1="train" \
+        --split2="dev" \
+        --folder="final"
+
+rm -rf ls_dump_split_1
+rm -rf ls_dump_split_2
+rm -rf ls_cv_train_dump
